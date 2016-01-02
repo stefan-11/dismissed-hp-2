e8f8d1019eea6eb0f4b50fa492c964cb65f9df63
Stefan <stefan-11@gmx.de>
2016-01-02 19:24:49 +0100

    static working

---
+++ galleries/galleries-json.js
@@ -0,0 +1,124 @@
+var galleriesObject = [
+{
+	"folder": "2015-10-10-Gallery-1",
+	"images": [
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1132-IMG_4677.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image1",
+		"sequenceIndex": "0"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1132-IMG_4678.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image2",
+		"sequenceIndex": "1"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-G0974877.jpg",
+		"w": "1200",
+		"h": "675",
+		"imageId": "image3",
+		"sequenceIndex": "2"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4679.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image4",
+		"sequenceIndex": "3"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4680.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image5",
+		"sequenceIndex": "4"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4681.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image6",
+		"sequenceIndex": "5"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4682.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image7",
+		"sequenceIndex": "6"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4683.jpg",
+		"w": "1200",
+		"h": "500",
+		"imageId": "image8",
+		"sequenceIndex": "7"
+	},
+	{
+		"src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1135-G0984879.jpg",
+		"w": "1200",
+		"h": "675",
+		"imageId": "image9",
+		"sequenceIndex": "8"
+	}
+	]
+},
+{
+	"folder": "2015-12-12-Gallery-2",
+	"images": [
+	{
+		"src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1058-IMG_4821.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image10",
+		"sequenceIndex": "0"
+	},
+	{
+		"src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1059-IMG_4834.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image11",
+		"sequenceIndex": "1"
+	},
+	{
+		"src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1059-IMG_4835.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image12",
+		"sequenceIndex": "2"
+	},
+	{
+		"src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1100-42.jpg",
+		"w": "640",
+		"h": "480",
+		"imageId": "image13",
+		"sequenceIndex": "3"
+	},
+	{
+		"src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1103-G0085125.jpg",
+		"w": "1200",
+		"h": "675",
+		"imageId": "image14",
+		"sequenceIndex": "4"
+	},
+	{
+		"src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1103-IMG_4851.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image15",
+		"sequenceIndex": "5"
+	},
+	{
+		"src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1116-IMG_4874.jpg",
+		"w": "1200",
+		"h": "900",
+		"imageId": "image16",
+		"sequenceIndex": "6"
+	}
+	]
+}
+]
