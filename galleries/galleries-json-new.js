d170450cf87236181472598e26cd4cb7b4bbb1a7
Stefan <stefan-11@gmx.de>
2016-01-04 19:38:10 +0100

    gallery for-loop changed to count from last element

---
+++ galleries/galleries-json-new.js
@@ -0,0 +1,216 @@
+var galleriesObject = [
+    {
+        "folder": "2015-05-09-Ihringen",
+        "headline": "2015-05-09-Ihringen",
+        "images": [
+            {
+                "src": "galleries/2015-05-09-Ihringen/11182167_889097607816411_3220136981146192681_n.jpg",
+                "w": 960,
+                "h": 639,
+                "imageId": "image0",
+                "sequenceIndex": 0
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/11205516_889097874483051_184882335378365186_n.jpg",
+                "w": 960,
+                "h": 639,
+                "imageId": "image1",
+                "sequenceIndex": 1
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/15601_889097554483083_5803499369689888559_n.jpg",
+                "w": 960,
+                "h": 639,
+                "imageId": "image2",
+                "sequenceIndex": 2
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/20150509_153508.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image3",
+                "sequenceIndex": 3
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/20150509_153515.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image4",
+                "sequenceIndex": 4
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/20150509_153523.jpg",
+                "w": 900,
+                "h": 1200,
+                "imageId": "image5",
+                "sequenceIndex": 5
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/20150509_153532.jpg",
+                "w": 900,
+                "h": 1200,
+                "imageId": "image6",
+                "sequenceIndex": 6
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/22325_889098027816369_8149577422043402326_n.jpg",
+                "w": 960,
+                "h": 640,
+                "imageId": "image7",
+                "sequenceIndex": 7
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/22627_888653541194151_134357913054800101_n.jpg",
+                "w": 960,
+                "h": 716,
+                "imageId": "image8",
+                "sequenceIndex": 8
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/IMG-20150510-WA0002.jpg",
+                "w": 1200,
+                "h": 675,
+                "imageId": "image9",
+                "sequenceIndex": 9
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/IMG-20150511-WA0001.jpg",
+                "w": 1200,
+                "h": 675,
+                "imageId": "image10",
+                "sequenceIndex": 10
+            },
+            {
+                "src": "galleries/2015-05-09-Ihringen/Ihringen.Poster.jpg",
+                "w": 678,
+                "h": 960,
+                "imageId": "image11",
+                "sequenceIndex": 11
+            }
+        ]
+    },
+    {
+        "folder": "2015-10-10-Gallery-1",
+        "headline": "2015-10-10-Gallery-1",
+        "images": [
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1132-IMG_4677.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image12",
+                "sequenceIndex": 0
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1132-IMG_4678.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image13",
+                "sequenceIndex": 1
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-G0974877.jpg",
+                "w": 1200,
+                "h": 675,
+                "imageId": "image14",
+                "sequenceIndex": 2
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4679.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image15",
+                "sequenceIndex": 3
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4680.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image16",
+                "sequenceIndex": 4
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4681.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image17",
+                "sequenceIndex": 5
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4682.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image18",
+                "sequenceIndex": 6
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1133-IMG_4683.jpg",
+                "w": 1200,
+                "h": 500,
+                "imageId": "image19",
+                "sequenceIndex": 7
+            },
+            {
+                "src": "galleries/2015-10-10-Gallery-1/SW-2015-06-15-1135-G0984879.jpg",
+                "w": 1200,
+                "h": 675,
+                "imageId": "image20",
+                "sequenceIndex": 8
+            }
+        ]
+    },
+    {
+        "folder": "2015-12-12-Gallery-2",
+        "headline": "2015-12-12-Gallery-2",
+        "images": [
+            {
+                "src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1058-IMG_4821.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image21",
+                "sequenceIndex": 0
+            },
+            {
+                "src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1059-IMG_4834.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image22",
+                "sequenceIndex": 1
+            },
+            {
+                "src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1059-IMG_4835.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image23",
+                "sequenceIndex": 2
+            },
+            {
+                "src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1100-42.jpg",
+                "w": 640,
+                "h": 480,
+                "imageId": "image24",
+                "sequenceIndex": 3
+            },
+            {
+                "src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1103-G0085125.jpg",
+                "w": 1200,
+                "h": 675,
+                "imageId": "image25",
+                "sequenceIndex": 4
+            },
+            {
+                "src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1103-IMG_4851.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image26",
+                "sequenceIndex": 5
+            },
+            {
+                "src": "galleries/2015-12-12-Gallery-2/SW-2015-06-16-1116-IMG_4874.jpg",
+                "w": 1200,
+                "h": 900,
+                "imageId": "image27",
+                "sequenceIndex": 6
+            }
+        ]
+    }
+]
